a87066639bcf95c4ed90655a4f44ba2c46c68be6
AhmedSaif0 <71180230+AhmedSaif0@users.noreply.github.com>
2023-07-02 16:29:20 +0400

    Vans page update

--- index.jsx
+++ index.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from './pages/Vans';
+import Vansdetails from './Vansdetails';
+
+import "./server"
 
 function App() {
   return (
@@ -11,11 +15,15 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/Vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/Vans" element={<Vans />} />
+        <Route path="/Vans/:id" element={<Vansdetails/>} />
+
       </Routes>
     </BrowserRouter>
   )
